304b0f15a2c3eb67d1bd351bc1f158577cdc5430
lucel79 <96133857+lucel79@users.noreply.github.com>
2026-06-08 20:09:18 -0300

    Delete src directory

--- src/servidor.js
+++
@@ -1,7 +0,0 @@
-import app from './turnos.js';
-
-process.loadEnvFile();
-
-app.listen(process.env.PUERTO || 3005, () => {
-    console.log(`servidor iniciado OK en puerto ${process.env.PUERTO}`);
-})
