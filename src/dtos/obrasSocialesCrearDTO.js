0e83dde1a99d613c37c389f2a8825f25df113a23
lucel79 <96133857+lucel79@users.noreply.github.com>
2026-06-08 20:41:33 -0300

    Add files via upload

---
+++ src/dtos/obrasSocialesCrearDTO.js
@@ -0,0 +1,10 @@
+export default class ObrasSocialesCrearDTO {
+
+    constructor(objeto){
+        this.id_obra_social = objeto.id_obra_social
+        this.nombre = objeto.nombre
+        this.descripcion = objeto.descripcion
+        this.porcentaje_descuento = objeto.porcentaje_descuento
+    }
+
+}
---
+++ src/servicios/pacientesServicio.js
@@ -0,0 +1,12 @@
+import Pacientes from "../db/pacientes.js";
+
+export default class PacientesServicio {
+
+    constructor(){
+        this.pacientes = new Pacientes();
+    }
+
+    buscarPorId = (id_paciente) => {
+        return this.pacientes.buscarPorId(id_paciente);
+    }
+}
