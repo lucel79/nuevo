250ec39fcd2c879d265e7b2e0fbb5a0fd749557c
lucel79 <96133857+lucel79@users.noreply.github.com>
2026-06-13 20:42:49 -0300

    Add files via upload

---
+++ src/servicios/usuariosServicio.js
@@ -0,0 +1,23 @@
+import Usuarios from "../db/usuarios";
+export default class UsuariosServicio {
+
+    constructor(){
+        this.usuarios = new Usuarios();
+    }
+
+    buscarPorId = (id_usuario) => {
+        return this.usuarios.buscarPorId(id_usuario);
+    }
+
+    buscar = (email, contrasenia) => {
+        return this.usuarios.buscar(email, contrasenia);
+    }
+
+    modificar = async (id_usuario, datos) => {
+        const existe = this.usuarios.buscarPorId(id_usuario);
+        if(!existe){
+            return null;
+        }
+        return this.usuarios.modificar(id_usuario, datos);
+    }
+}
