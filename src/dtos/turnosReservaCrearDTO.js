1819ecbdbdca1af40c6526a3b39410bf4a38a0a0
lucel79 <96133857+lucel79@users.noreply.github.com>
2026-06-15 19:31:21 -0300

    Delete src directory

--- src/dtos/turnosReservaCrearDTO.js
+++
@@ -1,9 +0,0 @@
-export default class TurnosReservasDTO {
-
-    constructor(objeto){
-        this.id_medico = objeto.id_medico
-        this.id_paciente = objeto.id_paciente
-        this.fecha_hora = objeto.fecha_hora
-    }
-
-}
